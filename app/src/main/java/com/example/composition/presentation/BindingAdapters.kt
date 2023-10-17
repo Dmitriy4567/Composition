fcaadd5dd1e801a3e7258e624ca56e9c40b4707f
Dmitriy <xxdimaxx@mail.ru>
2023-10-17 17:54:15 +0300

    Тут могу закосячить

--- app/src/main/java/com/example/composition/presentation/BindingAdapters.kt
+++ app/src/main/java/com/example/composition/presentation/BindingAdapters.kt
@@ -1,10 +1,18 @@
 package com.example.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.example.composition.R
 import com.example.composition.domain.entity.GameResult
+
+interface OnOptionClickListener{
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -33,26 +41,58 @@ fun bindRequiredPercentage(textView: TextView, percentage: Int) {
 @BindingAdapter("scorePercentage")
 fun bindscorePercentage(textView: TextView, gameResult: GameResult) {
     textView.text = String.format(
-        textView.context.getString(R.string.required_percentage),
+        textView.context.getString(R.string.score_percentage),
         getPercentOfRightAnswers(gameResult)
     )
 }
-private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult){
-    if (countOfQuestions == 0){
+
+private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
+    if (countOfQuestions == 0) {
         0
-    }else{
-        ((countOfRightAnswers / countOfQuestions.toDouble())* 100).toInt()
+    } else {
+        ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
     }
 }
 
 @BindingAdapter("resultEmoji")
-fun bindResultEmoji(imageView: ImageView, winner: Boolean){
+fun bindResultEmoji(imageView: ImageView, winner: Boolean) {
     imageView.setImageResource(getSmileResId(winner))
 }
-private fun getSmileResId(winner: Boolean): Int{
-    return if(winner){
+
+private fun getSmileResId(winner: Boolean): Int {
+    return if (winner) {
         R.drawable.ic_smile
-    }else{
+    } else {
         R.drawable.ic_sad
+    }
+}
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int){
+    textView.text = number.toString()
+}
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener){
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
     }
 }
